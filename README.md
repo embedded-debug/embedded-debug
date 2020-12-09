2a83243bcec2fd3c6ca5aeda8f3bb45051fb3bdc
Liangdi <wu@liangdi.me>
2020-12-10 02:08:55 +0800

    init project base vscode-mock-debug

---
+++ README.md
@@ -0,0 +1,4 @@
+# Embedded Debug
+
+基于 vscode 的 rust 语言嵌入式开发调试工具 (开发中)
+
---
+++ src/activateEmbeddedDebug.ts
@@ -0,0 +1,164 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+'use strict';
+
+import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+import { EmbeddedDebugSession } from './embeddedDebug';
+import { FileAccessor } from './debugRuntime';
+
+export function activateEmbeddedDebug(context: vscode.ExtensionContext, factory?: vscode.DebugAdapterDescriptorFactory) {
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.embedded-debug.runEditorContents', (resource: vscode.Uri) => {
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
+			if (targetResource) {
+				vscode.debug.startDebugging(undefined, {
+						type: 'embedded-debug',
+						name: 'Run File',
+						request: 'launch',
+						program: targetResource.fsPath
+					},
+					{ noDebug: true }
+				);
+			}
+		}),
+		vscode.commands.registerCommand('extension.embedded-debug.debugEditorContents', (resource: vscode.Uri) => {
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
+			if (targetResource) {
+				vscode.debug.startDebugging(undefined, {
+					type: 'embedded-debug',
+					name: 'Debug File',
+					request: 'launch',
+					program: targetResource.fsPath
+				});
+			}
+		}),
+		vscode.commands.registerCommand('extension.embedded-debug.toggleFormatting', (variable) => {
+			const ds = vscode.debug.activeDebugSession;
+			if (ds) {
+				ds.customRequest('toggleFormatting');
+			}
+		})
+	);
+
+	context.subscriptions.push(vscode.commands.registerCommand('extension.embedded-debug.getProgramName', config => {
+		return vscode.window.showInputBox({
+			placeHolder: "Please enter the name of a rust file in the workspace folder",
+			value: "readme.md"
+		});
+	}));
+
+	// register a configuration provider for 'embedded-debug' debug type
+	const provider = new MockConfigurationProvider();
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('embedded-debug', provider));
+
+	// register a dynamic configuration provider for 'embedded-debug' debug type
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('embedded-debug', {
+		provideDebugConfigurations(folder: WorkspaceFolder | undefined): ProviderResult<DebugConfiguration[]> {
+			return [
+				{
+					name: "Dynamic Launch",
+					request: "launch",
+					type: "embedded-debug",
+					program: "${file}"
+				},
+				{
+					name: "Another Dynamic Launch",
+					request: "launch",
+					type: "embedded-debug",
+					program: "${file}"
+				},
+				{
+					name: "Embedded Launch",
+					request: "launch",
+					type: "embedded-debug",
+					program: "${file}"
+				}
+			];
+		}
+	}, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
+
+	if (!factory) {
+		factory = new InlineDebugAdapterFactory();
+	}
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('embedded-debug', factory));
+	if ('dispose' in factory) {
+		context.subscriptions.push(factory);
+	}
+
+	// override VS Code's default implementation of the debug hover
+	/*
+	vscode.languages.registerEvaluatableExpressionProvider('markdown', {
+		provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
+			const wordRange = document.getWordRangeAtPosition(position);
+			return wordRange ? new vscode.EvaluatableExpression(wordRange) : undefined;
+		}
+	});
+	*/
+}
+
+class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'rust') {
+				config.type = 'embedded-debug';
+				config.name = 'Launch';
+				config.request = 'launch';
+				config.program = '${file}';
+				config.stopOnEntry = true;
+			}
+		}
+
+		if (!config.program) {
+			return vscode.window.showInformationMessage("Cannot find a program to debug").then(_ => {
+				return undefined;	// abort launch
+			});
+		}
+
+		return config;
+	}
+}
+
+export const workspaceFileAccessor: FileAccessor = {
+	async readFile(path: string) {
+		try {
+			const uri = vscode.Uri.file(path);
+			const bytes = await vscode.workspace.fs.readFile(uri);
+			const contents = Buffer.from(bytes).toString('utf8');
+			return contents;
+		} catch(e) {
+			try {
+				const uri = vscode.Uri.parse(path);
+				const bytes = await vscode.workspace.fs.readFile(uri);
+				const contents = Buffer.from(bytes).toString('utf8');
+				return contents;
+			} catch (e) {
+				return `cannot read '${path}'`;
+			}
+		}
+	}
+};
+
+class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
+
+	createDebugAdapterDescriptor(_session: vscode.DebugSession): ProviderResult<vscode.DebugAdapterDescriptor> {
+		return new vscode.DebugAdapterInlineImplementation(new EmbeddedDebugSession(workspaceFileAccessor));
+	}
+}
